0bd63798caba92b3b80526375d51d11fd5902188
Pixoll <pixoll.gamer@gmail.com>
2024-09-01 21:22:56 -0400

    Refactor

--- src/main.c
+++ src/main.c
@@ -4,107 +4,119 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_COMANDO 1024
+#define MAX_COMMAND 1024
 
-void ejecutar_comando(char **args) {
-    pid_t pid, wpid;
-    int estado;
-    pid = fork();
+void execute_command(char **args) {
+    const pid_t pid = fork();
+
+    // error
+    if (pid < 0) {
+        perror("fork");
+        return;
+    }
+
+    // child
     if (pid == 0) {
-        if(execvp(args[0], args) == -1) {
+        if (execvp(args[0], args) == -1) {
             perror("execvp");
         }
         exit(EXIT_FAILURE);
-    } else if(pid < 0) {
-        perror("fork");
-    } else {
-        do {
-            wpid = waitpid(pid, &estado, WUNTRACED);
-        } while (!WIFEXITED(estado) && !WIFSIGNALED(estado));
     }
+
+    // parent
+    int state;
+    do {
+        waitpid(pid, &state, WUNTRACED);
+    } while (!WIFEXITED(state) && !WIFSIGNALED(state));
 }
 
-void ejecutar_pipies(char **args) {
-    int pipefd[2];
-    pid_t pid;
+void execute_pipes(char **args) {
     int fd_in = 0;
 
     while (*args != NULL) {
-        pipe(pipefd);
-        pid = fork();
+        int pipe_fd[2];
+        pipe(pipe_fd);
+        const pid_t pid = fork();
 
+        // error
+        if (pid < 0) {
+            perror("fork");
+            return;
+        }
+
+        // child
         if (pid == 0) {
             dup2(fd_in, 0);
             if (*(args + 1) != NULL) {
-                dup2(pipefd[1], 1);
+                dup2(pipe_fd[1], 1);
             }
-            close(pipefd[0]);
+
+            close(pipe_fd[0]);
+
             if (execvp(*args, args) == -1) {
                 perror("execvp");
                 exit(EXIT_FAILURE);
             }
-        } else {
-            wait(NULL);
-            close(pipefd[1]);
-            fd_in = pipefd[0];
-            args++;
+
+            continue;
         }
+
+        // parent
+        wait(NULL);
+        close(pipe_fd[1]);
+        fd_in = pipe_fd[0];
+        args++;
     }
 }
 
-char **analizar_input(char* command) {
-    int tamaño_buffer = 64;
+char **parse_args(char *command) {
+    int buffer_size = 64;
     int i = 0;
-    char *arg;
-    char **args = malloc(tamaño_buffer * sizeof(char*));
 
-    arg = strtok(command, " \t\r\n");
+    char **args = malloc(buffer_size * sizeof(char *));
+    char *arg = strtok(command, " \t\r\n");
     while (arg != NULL) {
-        args[i] = arg;
-        i++;
-        if (i >= tamaño_buffer) {
-            tamaño_buffer += 64;
-            args = realloc(args, tamaño_buffer * sizeof(char*));
+        args[i++] = arg;
+
+        if (i >= buffer_size) {
+            buffer_size += 64;
+            args = realloc(args, buffer_size * sizeof(char *));
         }
+
         arg = strtok(NULL, " \t\r\n");
     }
+
     args[i] = NULL;
     return args;
 }
 
-char *leer_comando(void) {
-    char *comando = malloc(MAX_COMANDO * sizeof(char));
+char *read_command() {
+    char *command = malloc(MAX_COMMAND * sizeof(char));
     int c;
     int i = 0;
 
-    while ((c = getchar()) != '\n' && c != EOF) {
-        comando[i] = c;
-        i++;
-    }
-    comando[i] = '\0';  // Null-terminate the string
-    return comando;
+    while ((c = getchar()) != '\n' && c != EOF)
+        command[i++] = c;
+
+    command[i] = '\0';  // Null-terminate the string
+    return command;
 }
 
-int main(const int argc, const char **argv) {
-    char *input;
-    char **args;
-
+int main() {
     do {
         printf("> ");
-        input = leer_comando();
-        args = analizar_input(input);
+        char *input = read_command();
+        char **args = parse_args(input);
 
         if (args[0] != NULL) {
-            if (strchr(input, '|') != NULL) {
-                ejecutar_pipies(args);
-            } else {
-                ejecutar_comando(args);
-            }
+            if (strchr(input, '|') != NULL)
+                execute_pipes(args);
+            else
+                execute_command(args);
         }
 
         free(input);
         free(args);
-
     } while (1);
 
     return 0;
