2c99b982c8b7549535c381132458daa6d122ef90
Pixoll <pixoll.gamer@gmail.com>
2024-09-06 12:05:54 -0400

    Fancy print & allow "set reminder"

--- src/recordatorio.c
+++ src/recordatorio.c
@@ -6,20 +6,20 @@
 #include "prompt.h"
 
 int exec_set_recordatorio(const int argc, const char **argv) {
-    if (argc == 0 || argv[1] == NULL || strcmp(argv[1], "recordatorio") != 0) {
+    if (argc == 0 || argv[1] == NULL || (strcmp(argv[1], "recordatorio") != 0 && strcmp(argv[1], "reminder") != 0)) {
         fprintf(stderr, "set: command not found\n");
         return 1;
     }
 
     if (argc != 4) {
-        fprintf(stderr, "Usage: set recordatorio <seconds> \"<message>\"\n");
+        fprintf(stderr, "Usage: set recordatorio|reminder <seconds> \"<message>\"\n");
         return 1;
     }
 
     char *end;
     const int seconds = strtol(argv[2], &end, 10);
     if (end[0] != 0) {
-        fprintf(stderr, "set: invalid seconds\n");
+        fprintf(stderr, "set reminder: invalid seconds\n");
         return 1;
     }
 
@@ -32,7 +32,7 @@ int exec_set_recordatorio(const int argc, const char **argv) {
 
     if (pid == 0) {
         sleep(seconds);
-        printf("\nset recordatorio:\nReminder: %s\nSet %d seconds ago\n", argv[3], seconds);
+        printf("\nset reminder:\n\nReminder: \x1b[1;36m%s\x1b[0m\n\nSet: \x1b[1;33m%d seconds ago\x1b[0m\n", argv[3], seconds);
         prompt();
         exit(0);
     }
